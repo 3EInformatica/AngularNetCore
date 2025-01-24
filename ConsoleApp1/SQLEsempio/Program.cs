a51ecb56f1ee16abc9d7a1f9de16a9c7038ca371
atosraiprogetti <99817825+atosraiprogetti@users.noreply.github.com>
2025-01-24 12:24:17 +0100

    sql 2

--- ConsoleApp1/SQLEsempio/Program.cs
+++ ConsoleApp1/SQLEsempio/Program.cs
@@ -3,8 +3,11 @@
 using SQLEsempio.Models;
 using SQLEsempio.Services;
 
-List<Customer> listaClienti = DBServices.CaricaClientidaDB();
+//List<Customer> listaClienti = DBServices.CaricaClientidaDB();
+Console.WriteLine("Inserisci la lettera iniziale della ditta da cercare");  
+var lettera=Console.ReadLine();
 
+List<Customer> listaClienti =DBServices.CaricaClientidaDBFiltrato(lettera);
 foreach (var item in listaClienti)
 {
 
--- ConsoleApp1/SQLEsempio/Services/DBServices.cs
+++ ConsoleApp1/SQLEsempio/Services/DBServices.cs
@@ -11,7 +11,8 @@ namespace SQLEsempio.Services
 {
     internal class DBServices
     {
-        public static List<Customer> CaricaClientidaDB() {
+        public static List<Customer> CaricaClientidaDB()
+        {
             List<Customer> elenco = new List<Customer>();
             //collegare al DB
             using (SqlConnection conn = new SqlConnection(Costanti.ConnectionSting))
@@ -36,8 +37,47 @@ namespace SQLEsempio.Services
                 }
                 conn.Close();
             }
-           
 
+
+            return elenco;
+        }
+
+        public static List<Customer> CaricaClientidaDBFiltrato(string letteraIniziale)
+        {
+            List<Customer> elenco = new List<Customer>();
+            //collegare al DB
+            using (SqlConnection conn = new SqlConnection(Costanti.ConnectionSting))
+            {
+                conn.Open();
+                //var stato = conn.State;
+                //eseguire la query
+                //string sql = "select CustomerID,CompanyName,ContactName from Customers";
+                //sql += $" WHERE CompanyName LIKE '{letteraIniziale}%'";
+                //sql += " ORDER BY CompanyName";
+
+                string sql = @$"select CustomerID,CompanyName,ContactName from Customers
+                             WHERE CompanyName LIKE '{letteraIniziale}%'
+                            ORDER BY CompanyName";
+
+                using (SqlCommand cmd = new SqlCommand(sql, conn))
+                {
+                    var dr = cmd.ExecuteReader();
+                    //caricare in elenco il risultato della query
+                    while (dr.Read())
+                    {
+                        Customer c = new Customer();
+                        c.CustomerID = dr["CustomerID"].ToString();
+                        c.CompanyName = dr["CompanyName"].ToString();
+                        c.ContactName = dr["ContactName"].ToString();
+                        elenco.Add(c);
+                    }
+                    dr.Close();
+                }
+                conn.Close();
+            }
+
+
+            //return elenco.Where(s=>s.CompanyName.StartsWith(letteraIniziale)).ToList();
             return elenco;
         }
     }
